1809418f5e928754dca959abd6803fad32564214
huangyujing <huangyujing@sogou-inc.com>
2019-09-24 19:18:04 +0800

    bug fix

--- smarty/use_smarty.php
+++ smarty/use_smarty.php
@@ -1,11 +1,13 @@
 <?php
-    include("./smarty/libs/Smarty.class.php");
-    define('SMARTY_ROOT','./smarty/tpls');
+    if(!defined('APP_ROOT'))define('APP_ROOT',$_SERVER['DOCUMENT_ROOT']);
+    include(APP_ROOT."/smarty/libs/Smarty.class.php");
+
+    define('SMARTY_ROOT',APP_ROOT);
     $tpl = new Smarty();
     //文件路径
-    $tpl->template_dir = SMARTY_ROOT."/templates/";            //模板文件
-    $tpl->compile_dir = SMARTY_ROOT."/templates_c/";           //编译文件
-    $tpl->config_dir = SMARTY_ROOT."/configs/";                //配置文件
+    $tpl->template_dir = SMARTY_ROOT."/template/";            //模板文件
+    $tpl->compile_dir = SMARTY_ROOT."/template_c/";           //编译文件
+//    $tpl->config_dir = SMARTY_ROOT."/configs/";                //配置文件
     $tpl->cache_dir = SMARTY_ROOT."/cache/";                   //缓存文件
     $tpl->caching=1;
     $tpl->cache_lifetime=60*60*24;
